a2f35e2399cfe555892bb98b95f35d3d8a719d89
ssono <blusentinel32@gmail.com>
2017-10-02 12:47:40 -0400

    font changes

--- blog/static/js/base.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("li > a").hover(function(){
-        $(this).css("color", "rgb(0,153,153)");
-        }, function(){
-        $(this).css("color", "rgb(255, 191, 0)");
-    });
-});
